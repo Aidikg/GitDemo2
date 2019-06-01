11ad23ca4db56fcc8ae5c201268784d1f072fcda
Aidikg <aydi.biznes@mail.ru>
2019-06-01 16:26:40 -0500

    us guy commited

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -5,6 +5,11 @@ public class FirstClass {
 		System.out.println("My first Class");
 		System.out.println("I did some difference here");
 		System.out.println("After uploading ");
+		
+		// Here american guy did some changes
+		// it is From GitX2
+		
+		System.out.println("hello from gitX2");
 	}
 	
 }
--- src/SecondClass.java
+++ src/SecondClass.java
@@ -2,5 +2,11 @@
 public class SecondClass {
 	public static void main(String[] args) {
 		System.out.println("My second Class");
+		System.out.println("second Class from gitX2");
+		
+		
+		// This is american guy too
+		
+		
 	}
 }
